09fa302a5ab11cced2f8070ec63634667a9f3103
michelle <michelle.transbetter@gmail.com>
2022-06-15 17:14:44 +1000

    update nav bar

--- nextjs/components/nav.tsx
+++ nextjs/components/nav.tsx
@@ -3,16 +3,16 @@ import navStyles from '../styles/Nav.module.css';
 
 const Nav = () => {
     return (
-    <nav className={navStyles.nav}>
-        <ul>
-            <li>
-                <Link href="/subjects">Subjects</Link>
-            </li>
-            <li>
-                <Link href="/create">Create</Link>
-            </li>
-        </ul>
-    </nav>)
+        <nav className={navStyles.nav}>
+            <ul>
+                <li>
+                    <Link href="/subjects">Subjects</Link>
+                </li>
+                <li>
+                    <Link href="/create">Create</Link>
+                </li>
+            </ul>
+        </nav>)
 };
 
-export default Nav; 
+export default Nav;
